8e3ff25f47144b092a23ce818b3125ef91b623ef
aymanmohamed26 <ayman0106210@gmail.com>
2023-10-23 19:11:50 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { AsideComponent } from './component/aside/aside.component';
+import { ProductComponent } from './component/product/product.component';
+import { ProductCurdComponent } from './component/product-curd/product-curd.component';
+import { SlidesComponent } from './component/slides/slides.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    AsideComponent,
+    ProductComponent,
+    ProductCurdComponent,
+    SlidesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/component/product-curd/product-curd.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-curd',
+  templateUrl: './product-curd.component.html',
+  styleUrls: ['./product-curd.component.css']
+})
+export class ProductCurdComponent {
+  countryName:string = "Test Data";
+  countrytImage:string = "assets/images/349.jpg";
+countryDescription:string = "349.jpg";
+productDescription:string = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum illo facilis sint adipisci nihil voluptatem a molestiae sapiente iusto itaque, sit praesentium. Sunt necessitatibus iure omnis amet. Sapiente, alias est!";
+
+
+}
